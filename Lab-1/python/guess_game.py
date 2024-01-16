d37993355fca0fb99a62dc77400c3fc17bd7c00e
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2024-01-16 20:48:39 +0000

    added guessing game

---
+++ Lab-1/python/guess_game.py
@@ -0,0 +1,29 @@
+# function to simulate a guessing game by generating a random number
+import random
+
+
+def guessing_game():
+    target = random.randint(1, 100)
+    number_of_tries = 0
+    print(target)
+    print("Welcome to the Guessing Game!")
+    print("I'm thinking of a number between 1 and 100. Can you guess it?")
+    user_entry = int(input("Guess: "))
+
+    while user_entry != target:
+        number_of_tries += 1
+        if user_entry > target:
+            print("Good try, but that's too high. Try again")
+            user_entry = int(input("Guess: "))
+        else:
+            print("Good try, but that's too low. Try again")
+            user_entry = int(input("Guess: "))
+    else:
+        number_of_tries += 1
+        if number_of_tries == 1:
+            print(f"Yes! You guessed correctly after {number_of_tries} try! Congratulations.")
+        else:
+            print(f"Yes! You guessed correctly after {number_of_tries} tries! Congratulations.")
+
+
+guessing_game()
